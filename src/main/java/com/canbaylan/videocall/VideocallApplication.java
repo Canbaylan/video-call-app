c776601229cefabff9518ab905233bdabcd1cbb8
canbaylan <canbaylan34@gmail.com>
2023-09-15 14:47:32 +0300

    api added

--- src/main/java/com/canbaylan/videocall/VideocallApplication.java
+++ src/main/java/com/canbaylan/videocall/VideocallApplication.java
@@ -1,7 +1,11 @@
 package com.canbaylan.videocall;
 
+import com.canbaylan.videocall.service.UserService;
+import com.canbaylan.videocall.user.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideocallApplication {
@@ -10,4 +14,24 @@ public class VideocallApplication {
 		SpringApplication.run(VideocallApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(UserService service){
+		return args -> {
+			service.register(User.builder()
+							.username("Can")
+							.email("canbaylan@gmail.com")
+							.password("canpsw")
+					.build());
+			service.register(User.builder()
+					.username("Yunus")
+					.email("yun@gmail.com")
+					.password("yunpsw")
+					.build());
+			service.register(User.builder()
+					.username("burak")
+					.email("burak@gmail.com")
+					.password("burpsw")
+					.build());
+		};
+	}
 }
--- src/main/java/com/canbaylan/videocall/user/User.java
+++ src/main/java/com/canbaylan/videocall/user/User.java
@@ -1,2 +1,13 @@
-package com.canbaylan.videocall.user;public class User {
+package com.canbaylan.videocall.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class User {
+    private String username;
+    private String email;
+    private String password;
+    private String status;
 }
